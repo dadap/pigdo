d98000b92235f5205cba1aebe52e3a470cc9f35d
Daniel Dadap <daniel@dadap.net>
2017-07-13 07:07:00 -0500

    Apply GPLv2
    Jigdo is licensed under the GPLv2, so pigdo should be as well. Jigdo's
    license disclaimer does not contain the "any later version" language
    that permits upgrading from v2 of the GPL to a later version, so don't
    offer that option in pigdo's license, either.

--- jigdo.h
+++ jigdo.h
@@ -1,3 +1,20 @@
+/*
+ * pigdo: parallel implementation of jigsaw download
+ * Copyright (c) 2017 Daniel Dadap
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PIGDO_JIGDO_H
 #define PIGDO_JIGDO_H
 
