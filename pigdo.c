7f8e2d94295c7c478f8acb1e615db0673b9fc39c
Daniel Dadap <daniel@dadap.net>
2017-07-14 20:39:54 -0500

    Map the target image file
    After the .jigdo and .template file have been processed, we know enough
    to start putting the image together. Start off by just opening the
    image file and preparing for it to be written to.

--- pigdo.c
+++ pigdo.c
@@ -21,18 +21,50 @@
 #include <string.h>
 #include <assert.h>
 #include <inttypes.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
 
 #include "jigdo.h"
 #include "jigdo-template.h"
 #include "jigdo-md5.h"
+
+/**
+ * @brief Concatenate a directory and file name, with a '/' in between
+ *
+ * @param dir The name of the directory
+ * @param dir The name of the file
+ *
+ * @return @c dir/file on success, or NULL on error
+ */
+static char *dircat(const char *dir, const char *file)
+{
+    char *ret;
+    int len = strlen(dir) + strlen(file) + 2 /* '/' and '\0' */;
+
+    ret = calloc(len, 1);
+
+    if (ret) {
+        // asprintf(3) would be nice, but it's not part of any standard
+        if (snprintf(ret, len, "%s/%s", dir, file) != len - 1) {
+            free(ret);
+            ret = NULL;
+        }
+    }
+
+    return ret;
+}
 
 int main(int argc, const char * const * argv)
 {
     FILE *fp = NULL;
     jigdoData jigdo;
     templateDescEntry *table = NULL;
-    int count, ret = 1, len;
-    char *jigdoFile = NULL, *jigdoDir, *templatePath;
+    int count, ret = 1, fd = -1;
+    char *jigdoFile = NULL, *jigdoDir, *templatePath, *imagePath;
+    uint8_t *image = MAP_FAILED;
+    size_t imageLen = 0;
+    bool resize;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s jigdo-file-name\n", argv[0]);
@@ -62,23 +94,15 @@ int main(int argc, const char * const * argv)
     fclose(fp);
 
     jigdoDir = dirname(jigdoFile);
-    len = strlen(jigdoDir) + strlen(jigdo.templateName) + 2 /* '/' and '\0' */;
-    templatePath = calloc(len, 1);
+    templatePath = dircat(jigdoDir, jigdo.templateName);
     if (!templatePath) {
-        fprintf(stderr, "Failed to allocate memory for the template path.\n");
-        goto done;
-    }
-
-    // asprintf(3) would be nice, but it's not part of any standard
-    if (snprintf(templatePath, len, "%s/%s", jigdoDir, jigdo.templateName) !=
-        len - 1) {
-        fprintf(stderr, "Unexpected character count returned by snprintf\n");
-        free(templatePath);
+        fprintf(stderr, "Failed to build the template path.\n");
         goto done;
     }
 
     fp = fopen(templatePath, "r");
     free(templatePath);
+
     if (!fp) {
         fprintf(stderr, "Unable to open '%s' for reading\n", templatePath);
         goto done;
@@ -106,10 +130,38 @@ int main(int argc, const char * const * argv)
     assert(table[count-1].type == TEMPLATE_ENTRY_TYPE_IMAGE_INFO ||
            table[count-1].type == TEMPLATE_ENTRY_TYPE_IMAGE_INFO_OBSOLETE);
 
+    imageLen = table[count-1].u.imageInfo.size;
     printf("Image size is: %"PRIu64" bytes\n", table[count-1].u.imageInfo.size);
     printf("Image md5sum is: ");
     printMd5Sum(table[count-1].u.imageInfo.md5Sum);
     printf("\n");
+
+    imagePath = dircat(jigdoDir, jigdo.imageName);
+    fd = open(imagePath, O_RDWR | O_CREAT, 0644);
+    free(imagePath);
+    if (fd < 0) {
+        fprintf(stderr, "Failed to open image file '%s'\n", jigdo.imageName);
+        goto done;
+    }
+
+#if __APPLE__
+    /* Poor man's fallocate(2) - much slower than the real thing */
+    resize = (pwrite(fd, "\0", 1, imageLen-1) == 1);
+#else
+    resize = (posix_fallocate(fd, 0, imageLen) == 0);
+#endif
+    if (!resize) {
+        fprintf(stderr, "Failed to allocate disk space for image file\n");
+        goto done;
+    }
+
+    image = mmap(NULL, imageLen, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (image == MAP_FAILED) {
+        fprintf(stderr, "Failed to map image file into memory\n");
+        goto done;
+    }
+
+    msync(image, imageLen, MS_SYNC);
 
     ret = 0;
 
@@ -117,6 +169,14 @@ done:
     /* Clean up */
     if (fp) {
         fclose(fp);
+    }
+
+    if (image != MAP_FAILED) {
+        munmap(image, imageLen);
+    }
+
+    if (fd >= 0) {
+        close(fd);
     }
 
     free(jigdoFile);
